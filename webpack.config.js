a5787749f7108f0a4bf0bdae85f0c0342359ba26
DirectDesignLab <ianeom21@kookmin.ac.kr>
2025-01-31 10:11:40 +0900

    Build project and output to docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,41 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // 진입점 설정
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // 출력 디렉토리
+    path: path.resolve(__dirname, 'docs'), // 출력 디렉토리를 docs로 설정
     filename: 'bundle.js', // 출력 파일명
   },
   module: {
     rules: [
       {
-        test: /\.css$/, // CSS 파일 처리
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(js|jsx)$/, // JS와 JSX 파일 처리
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, // 이미지 파일 처리
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'], // 확장자 처리
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html', // HTML 템플릿 설정
-      filename: 'index.html', // 출력 HTML 파일명
+      template: './public/index.html',
+      filename: 'index.html',
     }),
   ],
   devServer: {
-    static: path.join(__dirname, 'dist'), // 정적 파일 제공 경로
+    static: path.join(__dirname, 'docs'),
     compress: true,
-    port: 3000, // 개발 서버 포트
+    port: 3000,
   },
+  devtool: 'source-map',
 };
